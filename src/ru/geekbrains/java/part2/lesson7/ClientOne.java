51d92e437f142c8b20813c22b38971b40d215059
Андрей <aiivashchenko@outlook.com>
2021-04-04 19:27:18 +0300

    part 2 lesson 7 homework

---
+++ src/ru/geekbrains/java/part2/lesson7/ClientOne.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.java.part2.lesson7;
+
+import ru.geekbrains.java.part2.lesson7.client.ChatClient;
+
+public class ClientOne {
+    public static void main(String[] args) {
+        new ChatClient();
+    }
+
+}
---
+++ src/ru/geekbrains/java/part2/lesson7/ClientTwo.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.java.part2.lesson7;
+
+import ru.geekbrains.java.part2.lesson7.client.ChatClient;
+
+public class ClientTwo {
+    public static void main(String[] args) {
+        new ChatClient();
+    }
+}
